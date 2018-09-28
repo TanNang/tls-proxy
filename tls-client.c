a84ad405641842f3d44ada40df7a252c2e1d61ed
zfl9 <j1002238565@gmail.com>
2018-09-28 13:37:21 +0800

    update on 2018-09-28 13:37:21

--- tls-client.c
+++ tls-client.c
@@ -43,3 +43,82 @@
            " -h                      show current message and exit\n")
 
 #define WEBSOCKET_STATUS_LINE "HTTP/1.1 101 Switching Protocols"
+
+/* 线程相关的 SSL 对象 */
+typedef struct {
+    pthread_t    tid;
+    SSL_CTX     *ctx;
+    SSL_SESSION *sess;
+} SSL_DATA;
+
+static int thread_nums = 0;
+static SSL_DATA *ssl_datas = NULL;
+
+/* 获取当前线程的 SSL_CTX */
+SSL_CTX *get_ssl_ctx() {
+    pthread_t tid = pthread_self();
+    for (int i = 0; i < thread_nums; ++i) {
+        if (ssl_datas[i].tid == tid) {
+            return ssl_datas[i].ctx;
+        }
+    }
+    return NULL;
+}
+
+/* 获取当前线程的 SSL_SESSION */
+SSL_SESSION *get_ssl_sess() {
+    pthread_t tid = pthread_self();
+    for (int i = 0; i < thread_nums; ++i) {
+        if (ssl_datas[i].tid == tid) {
+            return ssl_datas[i].sess;
+        }
+    }
+    return NULL;
+}
+
+// sizeof(ctime) = 20
+char *curtime(char *ctime) {
+    time_t rawtime;
+    time(&rawtime);
+
+    struct tm curtm;
+    localtime_r(&rawtime, &curtm);
+
+    sprintf(ctime, "%04d-%02d-%02d %02d:%02d:%02d",
+            curtm.tm_year + 1900, curtm.tm_mon + 1, curtm.tm_mday,
+            curtm.tm_hour,        curtm.tm_min,     curtm.tm_sec);
+
+    return ctime;
+}
+
+void set_tcp_sockopt(int sock) {
+    char ctime[20] = {0};
+    char error[64] = {0};
+
+    int optval = 1;
+    if (setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &optval, sizeof(optval)) == -1) {
+        printf("[%s] [WRN] setsockopt(TCP_NODELAY): (%d) %s\n", curtime(ctime), errno, strerror_r(errno, error, 64));
+    }
+
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1) {
+        printf("[%s] [WRN] setsockopt(SO_REUSEADDR): (%d) %s\n", curtime(ctime), errno, strerror_r(errno, error, 64));
+    }
+
+    if (setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, &optval, sizeof(optval)) == -1) {
+        printf("[%s] [WRN] setsockopt(SO_KEEPALIVE): (%d) %s\n", curtime(ctime), errno, strerror_r(errno, error, 64));
+    }
+
+    optval = 30;
+    if (setsockopt(sock, IPPROTO_TCP, TCP_KEEPIDLE, &optval, sizeof(optval)) == -1) {
+        printf("[%s] [WRN] setsockopt(TCP_KEEPIDLE): (%d) %s\n", curtime(ctime), errno, strerror_r(errno, error, 64));
+    }
+
+    if (setsockopt(sock, IPPROTO_TCP, TCP_KEEPINTVL, &optval, sizeof(optval)) == -1) {
+        printf("[%s] [WRN] setsockopt(TCP_KEEPINTVL): (%d) %s\n", curtime(ctime), errno, strerror_r(errno, error, 64));
+    }
+
+    optval = 3;
+    if (setsockopt(sock, IPPROTO_TCP, TCP_KEEPCNT, &optval, sizeof(optval)) == -1) {
+        printf("[%s] [WRN] setsockopt(TCP_KEEPCNT): (%d) %s\n", curtime(ctime), errno, strerror_r(errno, error, 64));
+    }
+}
